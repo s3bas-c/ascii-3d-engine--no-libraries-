c894bcd065b160eb57fc3acc2bd6c53b139faa5e
Sebastian Campillo <141476501+s3bas-c@users.noreply.github.com>
2026-08-11 10:24:26 -0700

    Add files via upload

---
+++ example.py
@@ -0,0 +1,27 @@
+from engine.screen import Screen
+from engine.projection import Projector
+from engine.lighting import Light3D
+from engine.obj_loader import Model_Converter
+import time
+
+screen = Screen(100, 40)
+projector = Projector()
+light = Light3D()
+converter = Model_Converter()
+
+mesh = converter.convert_obj_file("files/Models/cube.obj")
+
+mesh.calculate_center()
+
+screen.render()
+
+mesh.rotate(30,0,0)
+
+while(True):
+    lit_mesh = light.directional_light_prediction(mesh, direction=[-1,-1,-2])
+    proj_mesh = projector.perspective_proj(mesh=lit_mesh, width=100, height=40, scale=10, aspect_ratio=2, depth_scale=0.2, default_depth=-5)
+    screen.draw_faces(proj_mesh)
+    screen.render(show_errors=False)
+    mesh.rotate(0,-2,0)
+    time.sleep(0.03)
+    screen.clear()
